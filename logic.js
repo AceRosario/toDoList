a169b418aac3bab82d1943a5c39ab3502efb5cb7
Ace <haroldace12345@gmail.com>
2024-10-24 23:33:09 -0700

    second

--- logic.js
+++ logic.js
@@ -1,5 +1,15 @@
-function addText(stringOfText)
-{
-let input=document.getElementById("text").value;
+function addText(input) {
+   return input;
 }
 
+function outputText(text) {
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = `<h3>${text}</h3>`;
+}
+
+function submit() {
+    const inputField = document.getElementById('text');
+    const inputValue = inputField.value;
+    const processedText = addText(inputValue);
+    outputText(processedText);
+}
